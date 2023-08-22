8c98cac6a6bc3bd87afb5acc001db610d879487b
Pan <523141861@qq.com>
2023-08-22 22:32:34 +0800

    init

---
+++ blog/src/main/resources/sql/init.sql
@@ -0,0 +1,51 @@
+-- Active: 1692571432595@@127.0.0.1@5432@panzhipiao
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL, -- 密码应该经过哈希处理存储
+    email VARCHAR(255) UNIQUE,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+DROP TABLE IF EXISTS posts;
+CREATE TABLE posts (
+    id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users(id),
+    category_id INT,
+    title VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+DROP TABLE IF EXISTS comments;
+CREATE TABLE comments (
+    id SERIAL PRIMARY KEY,
+    post_id INT REFERENCES posts(id),
+    user_id INT REFERENCES users(id),
+    content TEXT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+DROP TABLE IF EXISTS categories;
+CREATE TABLE categories (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL UNIQUE,
+    description TEXT
+);
+
+ALTER TABLE posts ADD FOREIGN KEY (category_id) REFERENCES categories(id);
+
+DROP TABLE IF EXISTS tags;
+CREATE TABLE tags (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL UNIQUE
+);
+
+DROP TABLE IF EXISTS post_tags;
+CREATE TABLE post_tags (
+    post_id INT REFERENCES posts(id),
+    tag_id INT REFERENCES tags(id),
+    PRIMARY KEY (post_id, tag_id)
+);
